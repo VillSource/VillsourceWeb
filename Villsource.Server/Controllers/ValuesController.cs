a83b01df6301fde4e586a080182996241e3e3112
VillSource <anirut1800@gmail.com>
2025-03-09 03:48:45 +0700

    add name and version

---
+++ Villsource.Server/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Villsource.Server.Controllers;
+
+[Route("api")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    [HttpGet("name")]
+    public IActionResult GetName()
+    {
+        return Ok(new
+        {
+            value = "Villsource"
+        });
+    }
+
+    [HttpGet("version")]
+    public IActionResult GetVersion()
+    {
+        return Ok(new
+        {
+            value = "1.0.0"
+        });
+    }
+}
--- villsource.client/src/app/app.component.ts
+++ villsource.client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 
 interface WeatherForecast {
   date: string;
@@ -15,11 +16,14 @@ interface WeatherForecast {
 })
 export class AppComponent implements OnInit {
   public forecasts: WeatherForecast[] = [];
+  public name: string = '';
+  public version: string = '';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getForecasts();
+    this.getInfo();
   }
 
   getForecasts() {
@@ -31,6 +35,18 @@ export class AppComponent implements OnInit {
         console.error(error);
       }
     );
+  }
+
+  getInfo() {
+    this.name$.subscribe(n => this.name = n.value);
+    this.version$.subscribe(v => this.version = v.value);
+  }
+  get version$() {
+    return this.http.get('api/version').pipe(tap(console.log));
+  }
+
+  get name$() {
+    return this.http.get('api/name').pipe(tap(console.log));
   }
 
   title = 'villsource.client';
